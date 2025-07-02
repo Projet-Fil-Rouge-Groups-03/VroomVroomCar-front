39ddf52470d8e60b75a468a4816726ee4f6493c0
Pic Aurélia <pic.aurelia@gmail.com>
2025-07-02 15:07:54 +0200

    Add test to auth.spec + update App

--- src/app/core/services/auth.ts
+++ src/app/core/services/auth.ts
@@ -64,7 +64,7 @@ export class AuthService {
     return this.http.post(`${this.apiUrl}/logout`, {}, { withCredentials: true }).pipe(
       tap(() => {
         this.userSubject.next(null);
-        this.router.navigate(['']);
+        this.router.navigate(['/']);
       })
     );
   }
