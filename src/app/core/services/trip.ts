5a2e782e03ffffa27a10d46d983ddde91aac229b
Emikoya <pruvost.lily@gmail.com>
2025-07-03 11:07:33 +0200

    Mise en place TripService

--- src/app/core/services/trip.ts
+++ src/app/core/services/trip.ts
@@ -1,9 +1,52 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
+import { RequestTrip, Trip } from '../models/trip.model';
+import { VehiculeType } from '../models/company-car.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class Trip {
+export class TripService {
+  private apiURL = `${environment.apiUrl}/api/trips`;
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  // Create Trip (POST)
+  createTrip(trip: RequestTrip): Observable<Trip> {
+    return this.http.post<Trip>(`${this.apiURL}/create`, trip);
+  }
+  // Read Trip (GET)
+  getAllTrips(): Observable<Trip[]>{
+    return this.http.get<Trip[]>(this.apiURL);
+  }
+  getTripById(id : number) : Observable<Trip>{
+    return this.http.get<Trip>(`${this.apiURL}/${id}`);
+  }
+  // Update Trip (PUT)
+  updateTrip(id: number, trip : RequestTrip): Observable<Trip>{
+    return this.http.put<Trip>(`${this.apiURL}/update/${id}`, trip);
+  }
+  // Delete Trip (DELETE)
+  deleteTrip(id : number) : Observable<Trip>{
+    return this.http.delete<Trip>(`${this.apiURL}/${id}`);
+  }
+  // === GET + spécifiques === //
+  searchTrips(villeDepart?: string, villeArrivee?: string, dateDebutStr?: string, heureDepart?: string, vehiculeType: VehiculeType = VehiculeType.TOUS
+  ): Observable<Trip[]> {
+    let params = new HttpParams();
+    if (villeDepart) params = params.set('villeDepart', villeDepart);
+    if (villeArrivee) params = params.set('villeArrivee', villeArrivee);
+    if (dateDebutStr) params = params.set('dateDebutStr', dateDebutStr);
+    if (heureDepart) params = params.set('heureDepart', heureDepart);
+    if (vehiculeType && vehiculeType !== VehiculeType.TOUS) params = params.set('vehiculeType', vehiculeType);
+    return this.http.get<Trip[]>(`${this.apiURL}/search`, { params });
+  }
+  getUpcomingTrip(userId : number): Observable<Trip[]>{
+    return this.http.get<Trip[]>(`${this.apiURL}/upcoming/${userId}`);
+  }
+  getPastTrip(userId : number): Observable<Trip[]>{
+    return this.http.get<Trip[]>(`${this.apiURL}/past/${userId}`);
+  }
 }
