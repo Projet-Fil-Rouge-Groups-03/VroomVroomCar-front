a292015462484518c02128f3dbb86321b82851f0
Manax166 <paul.ripault@gmail.com>
2025-07-03 12:20:18 +0200

    user with user request

--- src/app/core/services/user.ts
+++ src/app/core/services/user.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
-import { User } from '../models/user.model';
+import { User, UserRequest  } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -25,17 +25,17 @@ export class UserService {
     return this.http.get<User>(`${this.apiUrl}/find-by-nom`, { params });
   }
 
-  createUser(user: User): Observable<User> {
-    return this.http.post<User>(`${this.apiUrl}/create-user`, user);
+  createUser(userRequest: UserRequest): Observable<User> {
+    return this.http.post<User>(`${this.apiUrl}/create-user`, userRequest);
   }
 
-  updateUserById(id: number, user: User): Observable<User> {
-    return this.http.put<User>(`${this.apiUrl}/edit-user/${id}`, user);
+  updateUserById(id: number, userRequest: UserRequest): Observable<User> {
+    return this.http.put<User>(`${this.apiUrl}/edit-user/${id}`, userRequest);
   }
 
-  updateUserByNom(nom: string, user: User): Observable<User> {
+  updateUserByNom(nom: string, userRequest: UserRequest): Observable<User> {
     const params = new HttpParams().set('nom', nom);
-    return this.http.put<User>(`${this.apiUrl}/edit-user-by-nom`, user, { params });
+    return this.http.put<User>(`${this.apiUrl}/edit-user-by-nom`, userRequest, { params });
   }
 
   deleteUser(id: number): Observable<string> {
