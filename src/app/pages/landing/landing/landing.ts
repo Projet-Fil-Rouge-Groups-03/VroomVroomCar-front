9da926835ef391bb701fcbedcc926268f4c28349
Emikoya <pruvost.lily@gmail.com>
2025-07-03 17:07:23 +0200

    template de toute beauté

--- src/app/pages/landing/landing/landing.ts
+++ src/app/pages/landing/landing/landing.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Hero } from '../hero/hero';
 
 @Component({
   selector: 'app-landing',
-  imports: [],
+  imports: [Hero],
   templateUrl: './landing.html',
   styleUrl: './landing.css'
 })
