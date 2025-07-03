b9c8c00603f8343fb81cd9012b5085b3e388065f
Pic Aurélia <pic.aurelia@gmail.com>
2025-07-03 11:01:04 +0200

    clean up

--- src/app/core/services/reservation.spec.ts
+++ src/app/core/services/reservation.spec.ts
@@ -1,7 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { provideHttpClient } from '@angular/common/http';
 import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
-
 import { ReservationService } from './reservation';
 import {ReservationRequest, Reservation } from '../models/reservation.model';
 import { environment } from '../../../environments/environment';
--- src/app/core/services/reservation.ts
+++ src/app/core/services/reservation.ts
@@ -1,5 +1,3 @@
-// src/app/services/reservation.service.ts
-
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
