4f28259df72b9107409f05892f17d8d3099c825a
Pic Aurélia <pic.aurelia@gmail.com>
2025-07-03 14:36:20 +0200

    Create navbar + correct guard

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,11 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, importProvidersFrom  } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideStore } from '@ngrx/store';
 
 import { routes } from './app.routes';
 import { reducers } from './core/store/reducers';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
+import { credentialsInterceptor } from './core/interceptors/credentials-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,5 +13,8 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideStore(reducers),
+    provideHttpClient(
+      withInterceptors([credentialsInterceptor])
+    ),
   ]
 };
