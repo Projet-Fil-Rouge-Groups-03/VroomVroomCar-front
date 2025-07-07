f4690edb9f68512e763cc478a6e8a9b68f81c074
Pic Aurélia <pic.aurelia@gmail.com>
2025-07-07 18:31:29 +0200

    Display add edit modale

--- src/app/pages/profile/modals/add-edit-carpooling-modal/add-edit-carpooling-modal.ts
+++ src/app/pages/profile/modals/add-edit-carpooling-modal/add-edit-carpooling-modal.ts
@@ -1,39 +1,70 @@
-import { Component, ElementRef, OnInit, output, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  output,
+  ViewChild,
+} from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-add-edit-carpooling-modal',
+  standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './add-edit-carpooling-modal.html',
-  styleUrl: './add-edit-carpooling-modal.css'
+  styleUrl: './add-edit-carpooling-modal.css',
 })
-export class AddEditCarpoolingModal implements OnInit{
-@ViewChild('carpoolingDetailsModal') myDialog!: ElementRef<HTMLDialogElement>;
+export class AddEditCarpoolingModal implements OnInit {
+  @ViewChild('carpoolingDetailsModal') myDialog!: ElementRef<HTMLDialogElement>;
 
   closed = output<void>();
-
-  saved = output<any>(); 
-
+  saved = output<any>();
+  heures: string[] = [];
   carpoolingForm!: FormGroup;
+  isEditMode = false;
+
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    // On initialise le formulaire
+    this.genererHeures(); 
     this.carpoolingForm = this.fb.group({
       dateDebut: ['', Validators.required],
       dateFin: ['', Validators.required],
       villeDepart: ['', Validators.required],
       lieuDepart: ['', Validators.required],
       villeArrivee: ['', Validators.required],
+      lieuArrivee: ['', Validators.required],
+      heureDepart: ['', Validators.required],
       vehiculePerso: [false],
-      vehiculeService: [null] 
-
+      vehiculeService: [null],
     });
   }
 
-  openModal(/* on pourrait passer des données ici pour l'édition */) {
-    // Si on est en mode édition, on pourrait patcher le formulaire ici
-    // this.carpoolingForm.patchValue(dataToEdit);
+  openModal(carpoolToEdit?: any) {
+    if (carpoolToEdit) {
+      this.isEditMode = true;
+      this.carpoolingForm.patchValue(carpoolToEdit);
+    } else {
+      this.isEditMode = false;
+      this.carpoolingForm.reset({
+        dateDebut: '',
+        dateFin: '',
+        villeDepart: '',
+        lieuDepart: '',
+        villeArrivee: '',
+        lieuArrivee: '',
+        heureDepart: '09:00',
+        vehiculePerso: false,
+        vehiculeService: null
+      });
+    }
+
+    // On ouvre la modale dans tous les cas
     this.myDialog.nativeElement.showModal();
   }
 
@@ -42,7 +73,7 @@ export class AddEditCarpoolingModal implements OnInit{
     this.closed.emit();
     this.carpoolingForm.reset();
   }
-  
+
   save() {
     if (this.carpoolingForm.invalid) {
       console.error('Formulaire invalide');
@@ -51,5 +82,12 @@ export class AddEditCarpoolingModal implements OnInit{
     }
     this.saved.emit(this.carpoolingForm.value);
     this.closeModal();
+  }
+
+  genererHeures() {
+    for (let i = 0; i < 24; i++) {
+      const heure = i.toString().padStart(2, '0');
+      this.heures.push(`${heure}:00`);
+    }
   }
 }
