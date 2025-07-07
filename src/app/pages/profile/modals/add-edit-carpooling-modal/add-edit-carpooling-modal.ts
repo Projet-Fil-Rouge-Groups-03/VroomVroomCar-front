912d2e7b63e87987473d0e43dc049e427c7b6bd4
Pic Aurélia <pic.aurelia@gmail.com>
2025-07-07 16:39:16 +0200

    Create AddEditCar modale

--- src/app/pages/profile/modals/add-edit-carpooling-modal/add-edit-carpooling-modal.ts
+++ src/app/pages/profile/modals/add-edit-carpooling-modal/add-edit-carpooling-modal.ts
@@ -1,11 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, output, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-edit-carpooling-modal',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './add-edit-carpooling-modal.html',
   styleUrl: './add-edit-carpooling-modal.css'
 })
-export class AddEditCarpoolingModal {
+export class AddEditCarpoolingModal implements OnInit{
+@ViewChild('carpoolingDetailsModal') myDialog!: ElementRef<HTMLDialogElement>;
 
+  closed = output<void>();
+
+  saved = output<any>(); 
+
+  carpoolingForm!: FormGroup;
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    // On initialise le formulaire
+    this.carpoolingForm = this.fb.group({
+      dateDebut: ['', Validators.required],
+      dateFin: ['', Validators.required],
+      villeDepart: ['', Validators.required],
+      lieuDepart: ['', Validators.required],
+      villeArrivee: ['', Validators.required],
+      vehiculePerso: [false],
+      vehiculeService: [null] 
+
+    });
+  }
+
+  openModal(/* on pourrait passer des données ici pour l'édition */) {
+    // Si on est en mode édition, on pourrait patcher le formulaire ici
+    // this.carpoolingForm.patchValue(dataToEdit);
+    this.myDialog.nativeElement.showModal();
+  }
+
+  closeModal() {
+    this.myDialog.nativeElement.close();
+    this.closed.emit();
+    this.carpoolingForm.reset();
+  }
+  
+  save() {
+    if (this.carpoolingForm.invalid) {
+      console.error('Formulaire invalide');
+      this.carpoolingForm.markAllAsTouched();
+      return;
+    }
+    this.saved.emit(this.carpoolingForm.value);
+    this.closeModal();
+  }
 }
