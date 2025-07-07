57ab8772a8f0d92bcb86f1f5cbf1bfd42d128c56
Pic Aurélia <pic.aurelia@gmail.com>
2025-07-07 15:07:41 +0200

    Create Filter Display

--- src/app/pages/carpooling/carpooling-filter/carpooling-filter.ts
+++ src/app/pages/carpooling/carpooling-filter/carpooling-filter.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, input, output } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-carpooling-filter',
-  imports: [],
+  imports: [FormsModule, CommonModule, ReactiveFormsModule],
   templateUrl: './carpooling-filter.html',
-  styleUrl: './carpooling-filter.css'
+  styleUrl: './carpooling-filter.css',
 })
 export class CarpoolingFilter {
+isVisible = input<boolean>(false);
+search = output<any>();
+
+  applyFilters(): void {
+    const filterData = {
+      villeDepart: 'Lille',
+      villeArrivee: 'Paris'
+    };
+    this.search.emit(filterData);
+  }
 
 }
