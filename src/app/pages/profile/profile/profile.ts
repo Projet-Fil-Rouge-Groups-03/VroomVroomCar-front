6ed4c41e1d5057fe38a768a2bbecba17f339c349
Manax166 <paul.ripault@gmail.com>
2025-07-07 16:19:43 +0200

    Premier jet Infos persos

--- src/app/pages/profile/profile/profile.ts
+++ src/app/pages/profile/profile/profile.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { Informations } from "../informations/informations";
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [Informations],
   templateUrl: './profile.html',
   styleUrl: './profile.css'
 })
 export class Profile {
-
+  
 }
