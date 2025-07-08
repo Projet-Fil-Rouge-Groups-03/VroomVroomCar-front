b9b64a55747a79579f19a1cf189e01f38ed5e018
Manax166 <paul.ripault@gmail.com>
2025-07-08 10:03:53 +0200

    User basculé sur profile

--- src/app/pages/profile/profile/profile.ts
+++ src/app/pages/profile/profile/profile.ts
@@ -3,6 +3,8 @@ import { Informations } from "../informations/informations";
 import { NextCarpools } from "../next-carpools/next-carpools";
 import { Notifications } from "../notifications/notifications";
 import { OldCarpools } from "../old-carpools/old-carpools";
+import { User } from '../../../core/models/user.model';
+import { AuthService } from '../../../core/services/auth';
 
 @Component({
   selector: 'app-profile',
@@ -14,5 +16,18 @@ import { OldCarpools } from "../old-carpools/old-carpools";
   },
 })
 export class Profile {
-  
+  user!: User | null;
+
+  constructor(private authService: AuthService){}
+
+  ngOnInit(){
+    this.authService.user$.subscribe({
+      next: (user) => {
+        this.user = user;
+      },
+      error: (err) => {
+        console.error('Erreur lors de la récupération de l’utilisateur :', err);
+      }
+    });
+  }
 }
