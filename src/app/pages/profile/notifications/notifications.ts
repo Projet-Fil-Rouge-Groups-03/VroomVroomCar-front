0032882c73343f5c3b88552f61ac9a615c9515ee
Manax166 <paul.ripault@gmail.com>
2025-07-09 11:53:55 +0200

    Bouton Voir + opérationnel

--- src/app/pages/profile/notifications/notifications.ts
+++ src/app/pages/profile/notifications/notifications.ts
@@ -12,14 +12,30 @@ import { User } from '../../../core/models/user.model';
 export class Notifications {
   user= input<User | null>();
   notifications!: Notification[];
+  visibleCount = 5;
+  visibleNotifications!: Notification[];
   constructor(private notificationService: NotificationService){}
 
   ngOnInit(){
-    this.notificationService.getUserNotifications(this.user()!.id).subscribe({
+    this.notificationService.getUserNotifications(this.user()!.id, 50).subscribe({
       next: (notifications) => {
         this.notifications = notifications;
+        console.log("length : " + notifications.length)
+        if(notifications.length >= this.visibleCount) this.visibleNotifications = notifications.slice(0, this.visibleCount);
+        else this.visibleNotifications = notifications;
       },
     })
-    console.log(this.notifications[0].date)
   }
+
+  loadMore() {
+    let previousCount = this.visibleCount;
+    if(this.visibleCount+5<=this.notifications.length){
+      this.visibleCount += 5;
+    } else {
+      this.visibleCount = this.notifications.length
+    }
+    console.log('previous value : ' + previousCount + "\nnew value : " +this.visibleCount + "\nnotifications length : " + this.notifications.length)
+    this.visibleNotifications = this.notifications.slice(previousCount, this.visibleCount);
+  }
+
 }
