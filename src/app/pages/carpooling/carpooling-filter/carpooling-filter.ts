cc4729629aac3ea84887883d1402fe60e9a7bcd8
Pic Aurélia <pic.aurelia@gmail.com>
2025-07-21 17:55:00 +0200

    init datas in filter

--- src/app/pages/carpooling/carpooling-filter/carpooling-filter.ts
+++ src/app/pages/carpooling/carpooling-filter/carpooling-filter.ts
@@ -1,33 +1,44 @@
 import { CommonModule } from '@angular/common';
-import { Component, input, output } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { Component, input, OnInit, output } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
 @Component({
   selector: 'app-carpooling-filter',
-  imports: [FormsModule, CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './carpooling-filter.html',
   styleUrl: './carpooling-filter.css',
 })
-export class CarpoolingFilter {
-isVisible = input<boolean>(false);
-search = output<any>();
+export class CarpoolingFilter implements OnInit {
+  isVisible = input<boolean>(false);
+  search = output<any>();
 
-// Propriétés pour les filtres
-  villeDepart = '';
-  villeArrivee = '';
-  dateDebutStr = '';
-  heureDepart = '00:00';
-  typeVehicule = 'Tout';
+  filterForm!: FormGroup;
+  today: string;
 
-  applyFilters(): void {
-    const filterData = {
-      villeDepart: this.villeDepart,
-      villeArrivee: this.villeArrivee,
-      dateDebutStr: this.dateDebutStr,
-      heureDepart: this.heureDepart,
-      typeVehicule: this.typeVehicule
-    };
-    this.search.emit(filterData);
+  readonly VEHICULE_SERVICE = 'VOITURE_SERVICE';
+  readonly VEHICULE_COVOIT = 'VOITURE_COVOIT';
+  readonly VEHICULE_TOUS = 'TOUS';
+  
+  constructor(private fb: FormBuilder) {
+    const date = new Date();
+    this.today = date.toISOString().split('T')[0];
+  }
+
+  ngOnInit(): void {
+    this.filterForm = this.fb.group({
+      villeDepart: [''],
+      villeArrivee: [''],
+      dateDebutStr: [this.today],
+      heureDepart: ['00:00'],
+      typeVehicule: [this.VEHICULE_TOUS], 
+    });
+    
+    this.filterForm.valueChanges.pipe(
+      debounceTime(400),
+      distinctUntilChanged()
+    ).subscribe(newValues => {
+      this.search.emit(newValues);
+    });
   }
 
 }
