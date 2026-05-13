ba9415e638ddc758396dd84a9d06c651b127d7fe
Manax166 <paul.ripault@gmail.com>
2026-05-13 11:05:30 +0200

    Bouton réservation de covoiturage fonctionnel + désactivation du bouton si la date d'un covoiturage est inférieure à la date du jour

--- src/app/pages/carpooling/modals/carpooling-details-modal/carpooling-details-modal.ts
+++ src/app/pages/carpooling/modals/carpooling-details-modal/carpooling-details-modal.ts
@@ -1,4 +1,4 @@
-import { Component, computed, effect, ElementRef, HostListener, inject, input, OnDestroy, output, signal, ViewChild } from '@angular/core';
+import { Component, computed, effect, ElementRef, HostListener, inject, input, OnDestroy, OnInit, output, signal, ViewChild } from '@angular/core';
 import { ModalParticipantsInformations } from '../../../../shared/components/modal-participants-informations/modal-participants-informations';
 import { ModalCarInformations } from '../../../../shared/components/modal-car-informations/modal-car-informations';
 import { Trip } from '../../../../core/models/trip.model';
@@ -8,6 +8,7 @@ import { SubscribeService } from '../../../../core/services/subscribe';
 import { Subject, takeUntil } from 'rxjs';
 import { Reservation } from '../../../../core/models/reservation.model';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../../../../core/services/auth';
 
 @Component({
   selector: 'app-carpooling-details-modal',
@@ -15,12 +16,13 @@ import { CommonModule } from '@angular/common';
   templateUrl: './carpooling-details-modal.html',
   styleUrl: './carpooling-details-modal.css',
 })
-export class CarpoolingDetailsModal implements OnDestroy {
+export class CarpoolingDetailsModal implements OnDestroy, OnInit {
   @ViewChild('carpoolingDetailsModal') myDialog!: ElementRef<HTMLDialogElement>;
 
   mode = input<'booking' | 'view-only'>('booking');
   organizer = input<string>('Organisateur inconnu');
-
+  user = inject(AuthService).currentUser;
+  unavailable : boolean = true;
   tripDetails = input<Trip | Reservation | undefined>();
   carForModalCarInfos = signal<Car | undefined>(undefined);
   subscribers = signal<Subscribe[]>([]);
@@ -60,7 +62,12 @@ export class CarpoolingDetailsModal implements OnDestroy {
       }
     });
   }
-
+  ngOnInit() {
+    if(this.tripDetails()?.dateFin == null) return true;
+    var str = this.tripDetails()!.dateFin;
+    this.unavailable = new Date(str) < new Date()
+    return;
+  }
   ngOnDestroy() {
     this.destroy$.next();
     this.destroy$.complete();
@@ -68,26 +75,35 @@ export class CarpoolingDetailsModal implements OnDestroy {
 
   // === BOUTON RESERVATION (non fini)===
   addSubscribe() {
-    // const currentTrip = this.tripDetails();
-    // if (currentTrip?.id) {
-    //   if(this.subscribeService.findByTrip(currentTrip.id)){
-    //     const subscribeRequest : SubscribeRequest = {
-    //       userId = ,
-    //       tripId = this.currentTrip.id,
-    //     };
-    //     this.subscribeService.create(requestSubscribe);
-    //   }
-    // }
+    if(!this.user) {
+      console.log("User invalide")
+      return;
+    } 
+    const currentTrip = this.tripDetails();
+    if (currentTrip?.id) {
+      if(this.subscribeService.findByTrip(currentTrip.id)){
+        const subscribeRequest : SubscribeRequest = {
+          userId : this.user.id,
+          tripId : currentTrip.id,
+        };
+        console.log("User " + this.user.nom + " a réservé le voyage " + currentTrip.id)
+        this.subscribeService.create(subscribeRequest)
+          .subscribe((value : Subscribe) =>{
+            console.log("User " + value.prenom + " " + value.nom + " est inscrit au voyage " + value.tripId);
+            this.closeModal()
+          });
+      }
+    }
   }
 
   // === OUVERTURE / FERMETURE DE LA MODALE ===
   closed = output<void>();
 
-openModal() {
-    if (this.myDialog && this.myDialog.nativeElement) {
-      this.myDialog.nativeElement.showModal();
+  openModal() {
+      if (this.myDialog && this.myDialog.nativeElement) {
+        this.myDialog.nativeElement.showModal();
+      }
     }
-  }
 
   closeModal() {
     if (this.myDialog?.nativeElement.open) {
